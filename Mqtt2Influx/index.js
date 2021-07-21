2a6886f3d5ec7087864bbf6c387af14349f6cb98
Tobias2 <donoghue1986@yahoo.de2>
2021-07-21 15:20:49 +0200

    added comments, refactoring, small fix

--- Mqtt2Influx/index.js
+++ Mqtt2Influx/index.js
@@ -5,7 +5,7 @@ const mqtt = require('mqtt');
  
 const client = mqtt.connect('mqtt://mosquitto');
  
-// create influx client
+// create influx client and define database schema
 const influx = new Influx.InfluxDB({
     host: 'influxdb',
     database: 'weather',
@@ -22,7 +22,8 @@ const influx = new Influx.InfluxDB({
       }
     ]
   });
-
+  
+  // finally create database if not already existing
   influx.getDatabaseNames()
   .then(names => {
     if (!names.includes('weather')) {
@@ -30,7 +31,7 @@ const influx = new Influx.InfluxDB({
     }
   }).catch(error => console.log({ error }));
 
-
+  // connect to mqtt and subscribe to any topic in the weather namespace
   client.on('connect', function () {
     client.subscribe('weather/#', function (err) {
         if(err) {
@@ -39,30 +40,36 @@ const influx = new Influx.InfluxDB({
         
       })
   });
-  // Connect to the mqtt broker and wait for messages
-  console.log(`Start listening to changing data`);
+
+  // Connect to the mqtt broker and wait for messages ...
+  console.log(`Start listening to changing data in MQTT`);
   client.on('message', function (topic, message) {
    
     console.log(`Topic: ${topic}, Message: ${message}`);
+    // ... to write them into the database
     writeDataToInflux(topic.split('/').pop(), JSON.parse(message));
     
-    //client.end()
   })
 
-
+  /**
+   * Writes the value to the database
+   * 
+   * @param {*} measuredTopic the influx db "table" to write to
+   * @param {*} messageObj The message object from mqtt contains the value and a timestamp
+   */
   function writeDataToInflux(measuredTopic, messageObj) {
     influx.writePoints([
         {
           measurement: measuredTopic,
           fields: { value: messageObj.value },
           tags: [],
-          timestamp: messageObj.timestamp,
+          timestamp: messageObj.timestamp
         }
       ], {
         database: 'weather',
-        precision: 's',
+        precision: 's'
       })
-      .catch(error => {
+      .catch(err => {
         console.error(`Error saving data to InfluxDB! ${err.stack}`)
       });
   }
